52a77d5867b03d4f9e43eafdaf05c568881f0752
McKay <wmsmckay@gmail.com>
2022-04-27 11:48:35 -0600

    added dynamic product pages url

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,22 @@
-let products = [];
+import ProductData from "./productData.js";
+import {getParam} from "./utils.js";
+
+const dataSource = new ProductData("tents");
+
+const productId = getParam("product");
+
+console.log(dataSource.findProductById(productId));
+                
+
+// let products = [];
 let checkout_items = [];
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
-}
+// function convertToJson(res) {
+//   if (res.ok) {
+//     return res.json();
+//   } else {
+//     throw new Error("Bad Response");
+//   }
+// }
 
 function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
@@ -17,13 +27,13 @@ function setLocalStorage(key, data) {
 }
 
 // get tents data
-function getProductsData() {
-  fetch("../json/tents.json")
-    .then(convertToJson)
-    .then((data) => {
-      products = data;
-    });
-}
+// function getProductsData() {
+//   fetch("../json/tents.json")
+//     .then(convertToJson)
+//     .then((data) => {
+//       products = data;
+//     });
+// }
 // or should we do it this way?
 // async function getProductsDataAwait() {
 //   products = await fetch("../json/tents.json").then(convertToJson);
@@ -39,12 +49,22 @@ function addToCart(e) {
   }
   //console.log(previous_products)
   //console.log(e.target.dataset.id)
-  const product = products.find((item) => item.Id === e.target.dataset.id);
-  checkout_items.push(product);
-  //console.log(previous_products);
+  
+
+
+  // const products = new ProductData("tents");
+  // const product = products.findProductById(e);
+
+
+  // console.log(products.getData1())
+  // console.log(products.getData());
+
+  // // product = products.find((item) => item.Id === e.target.dataset.id);
+  // checkout_items.push(product);
+  // //console.log(previous_products);
   setLocalStorage("so-cart", checkout_items);
 }
 
-getProductsData();
+// getProductsData();
 // add listener to Add to Cart button
 document.getElementById("addToCart").addEventListener("click", addToCart);
