3bc04a84e230b8888662659c58b39d35a7636050
McKay <wmsmckay@gmail.com>
2022-04-28 22:39:10 -0600

    finished step 05

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,15 @@
-// import ProductData from "./productData.js";
+import ProductData from "./productData.js";
 import ProductList from "./productList.js";
 
-// const productData = new ProductData("");
+// const productData = new ProductData("tents");
+const productCards = document.querySelector(".product-list");
 
+// Remove all of the current cards in the list.
+while (productCards.firstChild) {
+  productCards.removeChild(productCards.firstChild);
+}
 
-const productList = new ProductList("tents", "none");
+const productList = new ProductList("tents", productCards);
 
 productList.init();
+// productList.renderList();
--- src/js/productList.js
+++ src/js/productList.js
@@ -1,16 +1,45 @@
 import ProductData from "./productData";
 
 export default class ProductList {
-    constructor(category, htmlElement){
-        this.category = category;
-        this.htmlElement = htmlElement;
-        this.dataSource = {};
-        // this.list = {};
-    }
-    async init(){
-        this.dataSource = await new ProductData(this.category).getData();
-        console.log(this.dataSource);
-    }
-    populateCards(){}
-    displayCards(){}
+  constructor(category, listElement, dataSource) {
+    this.category = category;
+    this.listElement = listElement;
+    // console.log(listElement)
+    this.dataSource = {};
+    // this.list = {};
+  }
+  async init() {
+    this.dataSource = await new ProductData(this.category).getData();
+    // const list = await this.dataSource.getData();
+    // console.log(this.dataSource);
+    await this.renderList(this.dataSource);
+  }
+  renderList(data) {
+    const template = document.querySelector("#product-card-template");
+
+    data.forEach((product) => {
+      const clone = template.content.cloneNode(true);
+      const hydratedTemplate = this.prepareTemplate(clone, product);
+      this.listElement.appendChild(hydratedTemplate);
+    });
+  }
+  prepareTemplate(clone, product) {
+    const link = clone.querySelector(".product-card>a");
+    const image = clone.querySelector(".product-card>a>img");
+    const brand = clone.querySelector(".card__brand");
+    const name = clone.querySelector(".card__name");
+    const price = clone.querySelector(".product-card__price");
+
+    link.setAttribute("href", link.getAttribute("href") + product.Id);
+    image.setAttribute("src", product.Image);
+    image.setAttribute(
+      "alt",
+      image.getAttribute("alt") + product.NameWithoutBrand
+    );
+    brand.innerHTML = product.Brand.Name;
+    name.innerHTML = product.NameWithoutBrand;
+    price.innerHTML = price.innerHTML + product.FinalPrice;
+
+    return clone;
+  }
 }
