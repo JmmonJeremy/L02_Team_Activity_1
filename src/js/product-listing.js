6796695b097a6ab58e206175ed65fc4a4ec667ea
exttonymoraes <ext.tony.moraes@dafiti.com.br>
2022-05-06 13:57:33 -0300

    step 01 and 02

---
+++ src/js/product-listing.js
@@ -0,0 +1,14 @@
+import ProductData from "./productData.js";
+import ProductList from "./productList.js";
+// import { getLocalStorage } from "./utils.js";
+import { loadHeaderFooter } from "./utils.js";
+// import { displayCart } from "./cart-superscript.js";
+
+const dataSource = new ProductData("tents");
+const listElement = document.querySelector(".product-list");
+const dataList = new ProductList("tents", dataSource, listElement);
+
+dataList.init();
+
+// add the header and footer to main page
+loadHeaderFooter();
