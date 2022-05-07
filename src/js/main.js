9b68883a23c4978eed3b767d9f5952acdb8ad30c
McKay <wmsmckay@gmail.com>
2022-05-07 11:29:29 -0600

    added working newsletter form

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,50 @@ import { loadHeaderFooter } from "./utils.js";
 
 // add the header and footer to main page
 loadHeaderFooter();
+
+
+function newsLetter(){
+    document.addEventListener("DOMContentLoaded", () => {
+        document
+          .getElementById("news-signup")
+          .addEventListener("submit", handleForm);
+      });
+}
+
+function handleForm(ev) {
+    ev.preventDefault(); //stop the page reloading
+    let newsLetterForm = ev.target;
+    let fd = new FormData(newsLetterForm);
+    let json = convertFD2JSON(fd);
+
+    // Put in a real url here
+    let url = "http://localhost:3000/";
+    let header = new Headers();
+    header.append("Content-type", "application/json");
+    let req = new Request(url, {
+      headers: header,
+      body: json,
+      method: "POST",
+    });
+
+    console.log(req)
+
+    // Send the request to the server.
+    // fetch(req)
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log("Response from server");
+    //     console.log(data);
+    //   })
+    //   .catch(console.warn);
+  }
+
+function convertFD2JSON(formData) {
+    let obj = {};
+    for (let key of formData.keys()) {
+      obj[key] = formData.get(key);
+    }
+    return JSON.stringify(obj);
+  }
+
+newsLetter();
