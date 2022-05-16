c88e8504d81bbcc727936b2a8d4cdbca562df1a2
LawrenceYamete <LawrenceYamete>
2022-05-17 00:29:44 +0800

    Finished the Call to action-register with site.
    I used popup for subscription

---
+++ src/js/popup.js
@@ -0,0 +1,32 @@
+const popupScreen = document.querySelector(".popup-screen");
+const popupBox = document.querySelector(".popup-box");
+const popupBlur = document.querySelector(".popup-blur");
+const closebtn = document.querySelector(".close-btn");
+const websiteCookie = document.cookie.indexOf("SleepOutside=");
+
+
+// Popup Screen in 3 seconds after the page is loaded
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        popupScreen.classList.add("active");
+    }, 10000);
+    setTimeout(() => {
+        popupBlur.classList.add("active");
+    }, 10000);
+});
+
+
+// Close the popup Screen
+closebtn.addEventListener("click", () => {
+    popupScreen.classList.remove("active");
+
+    document.cookie = "SleepOutside=Website; max-age=" + 24 * 60 * 60;
+});
+
+if(websiteCookie != -1){
+    // Hide the popup screen if the cookie in not expired.
+    popupScreen.style.display = "none";
+} else {
+    // Show the popup screen if the cookie is expired.
+    popupScreen.style.display = "flex";
+}
