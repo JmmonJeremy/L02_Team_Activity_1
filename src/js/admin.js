5ada2a1ca6be4b5db6744789ba743cf93d64192b
McKay <wmsmckay@gmail.com>
2022-05-18 15:54:31 -0600

    hid the password from the user while entering it

--- src/js/admin.js
+++ src/js/admin.js
@@ -165,9 +165,9 @@ function loginForm() {
           >Password*
           <input
             id="myPassword"
-            type="text"
+            type="password"
             name="password"
-            placeholder="Bg147zxY"                       
+            placeholder="password"                       
             title="Password must contain at least 1 uppercase letter, 
             1 lowercase letter, 1 number, 1 special character 
             and be at least 8 characters long."
