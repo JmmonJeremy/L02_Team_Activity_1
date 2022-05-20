ac2c4e89a458b211e9b2ba28494ad1474d70180c
McKay <wmsmckay@gmail.com>
2022-05-19 21:07:04 -0600

    refactored popup to only showup if users has been to the site less than 1 time

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,30 +1,88 @@
+const mainElement = document.querySelector("main")
+const section = document.createElement("section")
 const popupScreen = document.querySelector(".popup-screen");
-const popupBox = document.querySelector(".popup-box");
 const popupBlur = document.querySelector(".popup-blur");
 const closebtn = document.querySelector(".close-btn");
-const websiteCookie = document.cookie.indexOf("SleepOutside=");
 
-// Popup Screen in 10 seconds after the page is loaded
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    popupScreen.classList.add("active");
-  }, 10000);
-  setTimeout(() => {
-    popupBlur.classList.add("active");
-  }, 10000);
-});
 
-// Close the popup Screen
-closebtn.addEventListener("click", () => {
-  popupScreen.classList.remove("active");
+section.setAttribute("id", "callToActionForm")
+section.innerHTML = popupHtml()
+mainElement.prepend(section);
 
-  document.cookie = "SleepOutside=Website; max-age=" + 24 * 60 * 60;
-});
+// Functions from util.js becuase they are not in a module
+function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
 
-if (websiteCookie != -1) {
-  // Hide the popup screen if the cookie in not expired.
-  popupScreen.style.display = "none";
-} else {
-  // Show the popup screen if the cookie is expired.
-  popupScreen.style.display = "flex";
+
+// Check how many visits the user has made and store a new one for every visit.
+let visits =  getLocalStorage("numberOfVisits");
+if (visits == null) {
+  visits = 0;
+}
+
+setLocalStorage("numberOfVisits", visits + 1)
+
+
+if (visits < 1) {
+  popupScreen.style.visibility = "visible"
+} 
+
+async function populatePopup() {
+  // Popup Screen in 10 seconds after the page is loaded
+  await window.addEventListener("load", () => {
+    setTimeout(() => {
+      popupScreen.classList.add("active");
+    }, 10000);
+    setTimeout(() => {
+      popupBlur.classList.add("active");
+    }, 10000);
+  });
+
+  // Close the popup Screen
+  await closebtn.addEventListener("click", () => {
+    popupScreen.classList.remove("active");
+    // section.remove()
+    // popupScreen.style.visibility = "hidden"
+  });
+}
+
+
+function popupHtml() {
+  return `<div class="popup-blur">
+      <div class="popup-screen">
+        <div class="popup-box">
+          <i class="close-btn">X</i>
+          <h2>Don't Forget to Subscribe!</h2>
+          <form name="news-signup" id="news-signup">
+            <div class="newsletter-container">
+              <div class="newsletter-fields">
+                <input
+                  type="text"
+                  autofocus
+                  placeholder="Name"
+                  name="name"
+                  maxlength="50"
+                  required
+                />
+                <input
+                  type="email"
+                  autofocus
+                  placeholder="Email"
+                  name="email"
+                  maxlength="50"
+                  required
+                />
+              </div>
+              <div>
+                <button id="submit">Subscribe</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>`
 }
