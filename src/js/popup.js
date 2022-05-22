969a897700cd593e81617897ff1938cf5af785ed
McKay <wmsmckay@gmail.com>
2022-05-21 22:16:14 -0600

    added functionality to close form after signing up

--- src/js/popup.js
+++ src/js/popup.js
@@ -5,14 +5,13 @@ section.setAttribute("id", "callToActionForm");
 section.innerHTML = popupHtml();
 mainElement.prepend(section);
 
-const popupScreen = document.querySelector(".popup-screen");
-const popupBlur = document.querySelector(".popup-blur");
-const closebtn = document.querySelector(".close-btn");
+
 
 // Functions from util.js becuase they are not in a module
 function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
+
 function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
@@ -23,36 +22,41 @@ if (visits == null) {
   visits = 0;
 }
 
+const popupScreen = document.querySelector(".popup-screen");
+// const popupBlur = document.querySelector(".popup-blur");
+
+
 if (visits <= 0) {
   popupScreen.style.visibility = "visible";
-  populatePopup;
+  populatePopup();
 }
+
 setLocalStorage("numberOfVisits", visits + 1);
 
-async function populatePopup() {
-  // Popup Screen in 10 seconds after the page is loaded
-  await window.addEventListener("load", () => {
-    setTimeout(() => {
-      popupScreen.classList.add("active");
-    }, 10000);
-    setTimeout(() => {
-      popupBlur.classList.add("active");
-    }, 10000);
-  });
+function populatePopup() {
 
   // Close the popup Screen
-  await closebtn.addEventListener("click", () => {
-    popupScreen.classList.remove("active");
-    // section.remove()
-    // popupScreen.style.visibility = "hidden"
-  });
+  document
+    .querySelector(".close-btn")
+    .addEventListener("click", () => {
+      popupScreen.classList.remove("active");
+      popupScreen.style.visibility = "hidden";
+    });
+
+  document
+    .querySelector("#submit-popup")
+    .addEventListener("submit", () => {
+      popupScreen.classList.remove("active");
+      popupScreen.style.visibility = "hidden";
+      alert("Thanks for subscribing!");
+    });
 }
 
 function popupHtml() {
   return `<div class="popup-blur">
       <div class="popup-screen">
         <div class="popup-box">
-          <i class="close-btn">X</i>
+          <button class="close-btn">X</button>
           <h2>Don't Forget to Subscribe!</h2>
           <form name="news-signup" id="news-signup">
             <div class="newsletter-container">
@@ -75,7 +79,7 @@ function popupHtml() {
                 />
               </div>
               <div>
-                <button id="submit">Subscribe</button>
+                <button id="submit-popup">Subscribe</button>
               </div>
             </div>
           </form>
